6a27db18eb1b3124d9cc152028e4afb230871764
JoaoAfranio <joaoafraniorosa@hotmail.com>
2022-11-15 12:21:23 -0300

    feat: input and output page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./assets/css/GlobalStyle";
+import InputRecord from "./pages/InputRecord";
 import Login from "./pages/Login";
+import OutputRecord from "./pages/OutputRecord";
 import Records from "./pages/Record";
 import Register from "./pages/Register";
 
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/records" element={<Records />} />
+          <Route path="/newInput" element={<InputRecord />} />
+          <Route path="/newOutput" element={<OutputRecord />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/OutputRecord.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import COLORS from "../constants/colors";
+
+function OutputRecord() {
+  return (
+    <Container>
+      <Tittle>Nova saída</Tittle>
+      <Input type="text" placeholder="Valor" />
+      <Input type="text" placeholder="Descrição" />
+      <Button>Salvar saída</Button>
+    </Container>
+  );
+}
+
+export default OutputRecord;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 25px;
+  background-color: ${COLORS.primary};
+
+  gap: 10px;
+`;
+
+const Tittle = styled.h1`
+  font-size: 26px;
+  font-weight: 700;
+  color: #ffffff;
+
+  margin-bottom: 20px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 15px;
+
+  border-radius: 5px;
+  border: none;
+  outline: none;
+
+  font-size: 20px;
+  color: #000000;
+
+  box-sizing: border-box;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  background-color: ${COLORS.secondary};
+  color: #ffffff;
+
+  border-radius: 5px;
+  border: none;
+
+  padding: 15px;
+
+  font-size: 20px;
+  font-weight: 700;
+`;
